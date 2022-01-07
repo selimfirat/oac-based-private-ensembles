daeb5da595bb94ebc9bead14963ac59602d3bc29
selimfirat <yilmazselimfirat@gmail.com>
2022-01-07 17:02:22 +0000

    remove infer.py

--- infer.py
+++
@@ -1,8 +0,0 @@
-from train import datasets
-
-for data_name in datasets.keys():
-    for fname in os.listdir("results/{data_name}/"):
-        if not fname.endswith(".pth"):
-            continue
-    
-    
